a35ed1d09f74480c92f60030d5249d735890d5b7
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-06-17 16:41:50 +0100

    fix constants

--- src/constans/index.js
+++ src/constans/index.js
@@ -14,7 +14,7 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
-export const CLOUDINERY = {
+export const CLOUDINARY = {
   CLOUD_NAME: 'CLOUD_NAME',
   API_KEY: 'API_KEY',
   API_SECRET: 'API_SECRET',
--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,17 +1,17 @@
-import cloudinery from 'cloudinary';
+import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
-import { CLOUDINERY } from '../constans/index.js';
+import { CLOUDINARY } from '../constans/index.js';
 import { getEnvVar } from './getEnvVar.js';
 
-cloudinery.v2.config({
+cloudinary.v2.config({
   secure: true,
-  cloud_name: getEnvVar(CLOUDINERY.CLOUD_NAME),
-  api_key: getEnvVar(CLOUDINERY.API_KEY),
-  api_secret: getEnvVar(CLOUDINERY.API_SECRET),
+  cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
+  api_key: getEnvVar(CLOUDINARY.API_KEY),
+  api_secret: getEnvVar(CLOUDINARY.API_SECRET),
 });
 
 export const saveFileToCloudinary = async (file) => {
-  const response = await cloudinery.v2.uploader.upload(file.path);
+  const response = await cloudinary.v2.uploader.upload(file.path);
   await fs.unlink(file.path);
   return response.secure_url;
 };
